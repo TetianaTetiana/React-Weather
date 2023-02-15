b3fadc3db1adefb839aac4948f848628fd3acf5b
Tetiana Karpenko <kuzia2016@i.ua>
2023-02-15 09:05:19 +0200

    feat: add application markup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-row">
+        <form id="search-form" action="">
+            <input id="search-text-input" type="search" placeholder="Enter a city" />
+            <input class="submit" type="submit" value="Search" />
+            <button class="real-temp">Current</button>
+        </form>
+          <div class="temp">
+            <p class="temperature">0</p>
+            <span class="units celsium current"> °C </span>
+            <span class="units fahrenheit off"> °F </span>
+        </div>
+      </div>
+        <p class="weather-description">Clear sky</p>
+        <div class="middle-row">
+            <div class="content">
+                <h1>
+                    City, Country
+                </h1>
+                <h2>
+                    Day, time
+                </h2>
+                <p class="wind">
+                    wind speed&nbsp;&nbsp;&nbsp;&nbsp;0 m/s
+                </p>
+                <p class="features">
+                    humidity&nbsp;&nbsp;&nbsp;&nbsp;0%<br />
+                </p>
+            </div>
+            <img class="img" src="https://cdn-icons-png.flaticon.com/512/1163/1163763.png" alt="weather-icon"></img>
+        </div>
+        <div class="footer-row">
+            <ul class="list">
+              <li class="item">
+                <div>Mon</div>
+                <p>🌤</p>
+                <span>10°C|</span>
+                <span>12°C</span>
+              </li>
+              <li class="item">
+                <div>Mon</div>
+                <p>🌤</p>
+                <span>10°C|</span>
+                <span>12°C</span>
+              </li>
+              <li class="item">
+                <div>Mon</div>
+                <p>🌤</p>
+                <span>10°C|</span>
+                <span>12°C</span>
+              </li>
+              <li class="item">
+                <div>Mon</div>
+                <p>🌤</p>
+                <span>10°C|</span>
+                <span>12°C</span>
+              </li>
+              <li class="item">
+                <div>Mon</div>
+                <p>🌤</p>
+                <span>10°C|</span>
+                <span>12°C</span>
+              </li>
+            </ul>
+        </div>
     </div>
   );
 }
-
 export default App;
