c4584429af24302d4426056ec4f65f7c48d6cae2
Tetiana Karpenko <kuzia2016@i.ua>
2023-02-28 12:38:08 +0200

    feat: add API Integration

--- src/App.js
+++ src/App.js
@@ -1,75 +1,103 @@
-
+import React, {useState} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="top-row">
-        <form id="search-form" action="">
-            <input id="search-text-input" type="search" placeholder="Enter a city" />
-            <input class="submit" type="submit" value="Search" />
-            <button class="real-temp">Current</button>
-        </form>
-          <div class="temp">
-            <p class="temperature">0</p>
-            <span class="units celsium current"> °C </span>
-            <span class="units fahrenheit off"> °F </span>
+  const [ready, setReady] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
+  const handleResp = (resp) => {
+    console.log(resp.data);
+    setWeatherData({
+      time: "12:00",
+      date: "monday",
+      temperature: resp.data.main.temp,
+      humidity: resp.data.main.humidity,
+      weatherDesc: resp.data.weather[0].description,
+      wind: resp.data.wind.speed,
+      city: resp.data.name,
+      iconUrl: `https://openweathermap.org/img/wn/${resp.data.weather[0].icon}@2x.png`,
+      description: resp.data.weather[0].description,
+    })
+    setReady(true);
+  }
+
+  if(ready){
+    return (
+      <div className="App">
+        <div className="top-row">
+          <form id="search-form" action="">
+              <input id="search-text-input" type="search" placeholder="Enter a city" />
+              <input className="submit" type="submit" value="Search" />
+              <button className="real-temp">Current</button>
+          </form>
+            <div className="temp">
+              <p className="temperature">{Math.round(weatherData.temperature)}</p>
+              <span className="units celsium current"> °C </span>
+              <span className="units fahrenheit off"> °F </span>
+          </div>
         </div>
+          <p className="weather-description">{weatherData.weatherDesc}</p>
+          <div className="middle-row">
+              <div className="content">
+                  <h1>
+                      {weatherData.city}
+                  </h1>
+                  <h2>
+                      {weatherData.date}, {weatherData.time}
+                  </h2>
+                  <p className="wind">
+                      wind speed&nbsp;&nbsp;&nbsp;&nbsp;{weatherData.wind} m/s
+                  </p>
+                  <p className="features">
+                      humidity&nbsp;&nbsp;&nbsp;&nbsp;{weatherData.humidity}%<br />
+                  </p>
+              </div>
+              <img className="img" src={weatherData.iconUrl} alt={weatherData.description}></img>
+          </div>
+          <div className="footer-row">
+              <ul className="list">
+                <li className="item">
+                  <div>Mon</div>
+                  <p>🌤</p>
+                  <span>10°C|</span>
+                  <span>12°C</span>
+                </li>
+                <li className="item">
+                  <div>Mon</div>
+                  <p>🌤</p>
+                  <span>10°C|</span>
+                  <span>12°C</span>
+                </li>
+                <li className="item">
+                  <div>Mon</div>
+                  <p>🌤</p>
+                  <span>10°C|</span>
+                  <span>12°C</span>
+                </li>
+                <li className="item">
+                  <div>Mon</div>
+                  <p>🌤</p>
+                  <span>10°C|</span>
+                  <span>12°C</span>
+                </li>
+                <li className="item">
+                  <div>Mon</div>
+                  <p>🌤</p>
+                  <span>10°C|</span>
+                  <span>12°C</span>
+                </li>
+              </ul>
+          </div>
+          <p className="githublink">Coded by Tetiana Karpenko. Open-sourced on <a href="https://github.com/TetianaTetiana">GitHub</a></p>
       </div>
-        <p class="weather-description">Clear sky</p>
-        <div class="middle-row">
-            <div class="content">
-                <h1>
-                    City, Country
-                </h1>
-                <h2>
-                    Day, time
-                </h2>
-                <p class="wind">
-                    wind speed&nbsp;&nbsp;&nbsp;&nbsp;0 m/s
-                </p>
-                <p class="features">
-                    humidity&nbsp;&nbsp;&nbsp;&nbsp;0%<br />
-                </p>
-            </div>
-            <img class="img" src="https://cdn-icons-png.flaticon.com/512/1163/1163763.png" alt="weather-icon"></img>
-        </div>
-        <div class="footer-row">
-            <ul class="list">
-              <li class="item">
-                <div>Mon</div>
-                <p>🌤</p>
-                <span>10°C|</span>
-                <span>12°C</span>
-              </li>
-              <li class="item">
-                <div>Mon</div>
-                <p>🌤</p>
-                <span>10°C|</span>
-                <span>12°C</span>
-              </li>
-              <li class="item">
-                <div>Mon</div>
-                <p>🌤</p>
-                <span>10°C|</span>
-                <span>12°C</span>
-              </li>
-              <li class="item">
-                <div>Mon</div>
-                <p>🌤</p>
-                <span>10°C|</span>
-                <span>12°C</span>
-              </li>
-              <li class="item">
-                <div>Mon</div>
-                <p>🌤</p>
-                <span>10°C|</span>
-                <span>12°C</span>
-              </li>
-            </ul>
-        </div>
-        <p class="githublink">Coded by Tetiana Karpenko. Open-sourced on <a href="https://github.com/TetianaTetiana">GitHub</a></p>
-    </div>
-  );
+    );
+  }else{
+    const apiKey = "33efe89f103feda08ec5412af8a983d6";
+    let city = "Kyiv";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(handleResp);
+
+    return "Loading data..."
+  }
 }
 export default App;
